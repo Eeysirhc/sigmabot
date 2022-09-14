0870913f184a86ea42f25c42b616249d4f9c88a7
TremendouslyHighFrequency <112732047+TremendouslyHighFrequency@users.noreply.github.com>
2022-09-14 09:29:43 -0700

    Update faq.py
    change wallets yoroi modifier to be .yoroi break out

--- faq.py
+++ faq.py
@@ -60,10 +60,6 @@ def faq_wallets(modifier=""):
 		df = """
 		In this tutorial you will find instructions on how to install the cold mobile wallet: https://www.youtube.com/watch?v=7q3Jq_OvhKY
 		"""
-	elif modifier=="yoroi":
-		df = """
-		What are you, some sort of cave person living under a rock? Use a wallet listed here: https://ergoplatform.org/en/get-erg/#Wallets
-		"""
 	else:
 		df = """
 		https://ergoplatform.org/en/get-erg/#Wallets
@@ -313,5 +309,12 @@ def faq_drama():
 def faq_contribute():
 	df = """
 	https://docs.ergoplatform.com/contribute/
+	"""
+	return(df)
+
+# YOROI
+def faq_yoroi():
+	df = """
+	What are you, some sort of cave person living under a rock? Use a wallet listed here: https://ergoplatform.org/en/get-erg/#Wallets
 	"""
 	return(df)
